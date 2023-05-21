9566e18695a7262d15cfb730124ff27eae4e4864
yejunho10 <hyjcompany30@naver.com>
2023-05-21 10:03:41 +0900

    refactor: Style Code

--- src/main/java/net/starly/skinbook/command/tabcomplete/SkinBookTab.java
+++ src/main/java/net/starly/skinbook/command/tabcomplete/SkinBookTab.java
@@ -3,21 +3,20 @@ package net.starly.skinbook.command.tabcomplete;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@SuppressWarnings("all")
 public class SkinBookTab implements TabCompleter {
+
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
         if (args.length == 1) {
             return Arrays.asList("도움말", "생성");
         } else if (args.length == 2) {
-            if (Arrays.asList("생성", "create").contains(args[0])) {
-                return Arrays.asList("1", "2", "3", "4", "5");
-            }
+            if (Arrays.asList("생성", "create").contains(args[0])) return Arrays.asList("1", "2", "3", "4", "5");
         }
 
         return Collections.emptyList();
